8d7a8169c4cd45044fe172f2213480e2312b0570
watson <watson@okmaster.com>
2020-12-29 11:39:02 +0800

    if use echarts > 5.0, just add require('echarts') in the specific component after install echart npm, no need other step

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
+  <div class="about" id="about">
     <h1>This is an about page</h1>
+    <div id="main" style="width: 600px;height:400px;"></div>
   </div>
 </template>
+<script>
+var echarts = require('echarts');
+export default {
+  name: "app",
+  data() {
+    return {
+      option: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "pie",
+            data: [19, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    drawPid(id) {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById(id));
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(this.option);
+    }
+  },
+  mounted() {
+    this.$nextTick(function() {
+      this.drawPid("main");
+    });
+  }
+};
+</script>
